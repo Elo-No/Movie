0d148cf8af45b594198c934bb5bf0fa5a95e151b
Elo-No <eleno.nouri@gmail.com>
2021-10-05 08:44:22 +0330

    add post()

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,18 +3,28 @@ from .serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view()
+
+@api_view(['GET', 'POST'])
 def MovieListView(request):
+    if request.method == 'GET':
         movies = Movie.objects.all()
-        serializers = MovieSerializer(movies,many=True)
-        print('movies = ',movies)
-        print('serializers = ',serializers)
+        serializers = MovieSerializer(movies, many=True)
+        print('movies = ', movies)
+        print('serializers = ', serializers)
         return Response(serializers.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error)
+
 
 @api_view()
-def MovieDetailView(request,pk):
-        movie = Movie.objects.get(pk=pk)
-        serializers = MovieSerializer(movie)
-        print('movie = ',movie)
-        print('serializers = ',serializers)
-        return Response(serializers.data)
+def MovieDetailView(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializers = MovieSerializer(movie)
+    print('movie = ', movie)
+    print('serializers = ', serializers)
+    return Response(serializers.data)
