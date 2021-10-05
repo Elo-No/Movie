0cc1c3ab50cb52b562abf306fed436dc4ac00a0e
Elo-No <eleno.nouri@gmail.com>
2021-10-05 20:36:23 +0330

    add new model

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
 
 class StreamPlatform(models.Model):
@@ -19,3 +19,14 @@ class Watchlist(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    description = models.CharField(max_length=200,null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    Watchlist = models.ForeignKey(Watchlist,on_delete=models.CASCADE,related_name="review")
+
+    def __str__(self):
+        return str(self.rating) + "-" + self.Watchlist.title
