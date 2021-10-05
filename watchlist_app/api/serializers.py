1ab33b8b98709d46d4dbc85a01ec1aeb9dd0cd50
Elo-No <eleno.nouri@gmail.com>
2021-10-05 09:37:03 +0330

    add update()

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -5,7 +5,14 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
-    
+
     def create(self,validate_data):
         return Movie.objects.create(**validate_data)
+    
+    def update(self,instance,validate_data):
+        instance.name = validate_data.get('name',instance.name) 
+        instance.description = validate_data.get('description',instance.description) 
+        instance.active = validate_data.get('active',instance.active) 
+        instance.save()
+        return instance
     
